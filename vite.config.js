613e81fdc5e2f815ff325ab2705e78dc917a4a64
Syed-0707 <afrith3007@gmail.com>
2026-07-07 14:06:50 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  // base: '/smart-bharat-ai-civic-companion/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build'
+    ? '/smart-bharat-ai-civic-companion/'
+    : '/',
 
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+}))
